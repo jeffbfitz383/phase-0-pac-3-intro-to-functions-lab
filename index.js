4d687cd02d01c76ebe90690d69f5e15e53fd021a
Jeffrey <jefbfitzp33@outlook.com>
2023-09-16 21:04:15 -0700

    complete lab  git push
    '

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string .toUpperCase();
+  }
+
+  function whisper(string) {
+    return string .toLowerCase();
+  }
+
+  function logShout(string) {
+console.log( string.toUpperCase());
+
+  }
+
+  function logWhisper(string) { 
+    console.log( string.toLowerCase());
+    
+      }
+
+
+function sayHiToHeadphonedRoommate(string) {
+let message;
+    if (string === string.toLowerCase()) {
+    message = "I can't hear you!";
+    
+
+    } else if (string === string.toUpperCase()) {
+message = "YES INDEED!";   
+    } else if (string === "Let's have dinner together!") {
+        message = "I would love to!";   }
+    
+
+return message;
+
+}
